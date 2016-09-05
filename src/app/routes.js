cba48e4771788fe89ab0fc0cbcb67af57c22802e
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-05 17:23:41 +0700

    Update routes.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-// components
+// layout: MainApp
 import MainApp from './components/layout/MainApp'
 
-// Home : components
+// home : components
 import Home from './components/home/Home'
+
+// todo: Todo
+import Todo from './components/todo/Todo'
 
 export default () => {
 	return (
 		<Router history={browserHistory}>
-			<Route path='/' component={MainApp}>
+			<Route path='/'
+				component={MainApp}>
 				<IndexRoute component={Home} />
+				<Route path='home'
+					component={Home} />
+				<Route path='todo'
+					component={Todo}
+					title="Todo" />
 			</Route>
 		</Router>
 	)
