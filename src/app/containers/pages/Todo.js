b0f7e6c31cfe7568aa750da82cbd7421e6aae72a
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-13 18:42:37 +0700

    Update Todo.js

--- src/app/containers/pages/Todo.js
+++ src/app/containers/pages/Todo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import render from 'react-dom'
 
 import Header from '../../components/todo/Header'
@@ -9,33 +9,48 @@ import { connect } from 'react-redux'
 import store from '../../store'
 import { getAllTodo } from '../../actions/todoAction'
 
-// Mapping state to property
-const mapStateToProps = function(store) {
-	console.log(store.todoState)
+// mapping the state is that contains in store to {this.props}
+const mapStateToProps = (store) => {
 	return {
-		empDatas: store.todoState.users
+		empDatas: store.todoState
+	}
+}
+
+// dispatch an action will be called reducer mapping it to {this.props}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onPageLoad: () => {
+			dispatch(getAllTodo())
+		},
+		onTodoClick: () => {
+			dispatch(getAllTodo())
+		}
 	}
 }
 
 class Todo extends Component {
-	constructor (props, context) {
-		super(props, context)
+	constructor (props) {
+		super(props)
 		this.title = 'Learning getting the data from the database'
 	}
 
+	static propTypes: {
+        empDatas: PropTypes.array.isRequired,
+		onPageLoad: PropTypes.func.isRequired
+    }
+
 	componentDidMount () {
-		store.dispatch(getAllTodo())
+		// this.props.onPageLoad()
 	}
 
 	render () {
 		return (
 			<div>
 				<Header title={this.title} />
-				<PageData />
+				<PageData empDatas={this.props.empDatas} onPageLoad={this.props.onPageLoad} />
 			</div>
 		)
 	}
 }
 
-export default connect(mapStateToProps)(Todo)
-// export default Todo
+export default connect(mapStateToProps, mapDispatchToProps)(Todo)
