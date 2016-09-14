d6fd1523c24b6607e4b683b2a77d69a3508fa156
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-14 14:47:06 +0700

    Refactoring code to ES6

--- src/app/containers/pages/Todo.js
+++ src/app/containers/pages/Todo.js
@@ -4,19 +4,35 @@ import render from 'react-dom'
 import Header from '../../components/todo/Header'
 import PageData from '../../components/todo/PageData'
 
-// Redux
 import { connect } from 'react-redux'
-import { getAllTodo, addSingleTodo } from '../../actions/todoAction'
 
-// mapping the state is that contains in store to {this.props}
+/*
+	Imports Redux action creator
+*/
+import {
+	getAllTodo,
+	addSingleTodo
+}
+from '../../actions/todoAction'
+
+/*
+	component will subscribe to Redux store updates.
+	Any time it updates, mapStateToProps will be called.
+	Its result must be a plain object*,
+	and it will be merged into the component’s props
+*/
 const mapStateToProps = (state) => {
-	console.log(state)
 	return {
 		empDatas: state.todoState
 	}
 }
 
-// dispatch an action will be called reducer mapping it to {this.props}
+/*
+	object is passed, each function inside it will be assumed to be a Redux action creator.
+	An object with the same function names,
+	but with every action creator wrapped into a dispatch call so they may be invoked directly,
+	will be merged into the component’s props
+*/
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onPageLoad: () => {
@@ -32,18 +48,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class Todo extends Component {
-	constructor (props) {
-		super(props)
-		this.title = 'Learning getting the data from the database'
-	}
-
-	// static propTypes: {
-	// 	empDatas: PropTypes.array.isRequired,
-	// 	onPageLoad: PropTypes.func.isRequired
-	// }
-
-	componentDidMount () {
-		// this.props.onPageLoad()
+	constructor (props, context) {
+		super(props, context)
 	}
 
 	onRefreshClick = () => {
@@ -57,8 +63,14 @@ class Todo extends Component {
 	render () {
 		return (
 			<div>
-				<Header title={this.title} onRefreshClick={this.onRefreshClick} onAddSingleTodo={this.onAddSingleTodo} />
-				<PageData empDatas={this.props.empDatas} onPageLoad={this.props.onPageLoad} />
+				<Header
+					title={this.title}
+					onRefreshClick={this.onRefreshClick}
+					onAddSingleTodo={this.onAddSingleTodo} />
+
+				<PageData
+					empDatas={this.props.empDatas}
+					onPageLoad={this.props.onPageLoad} />
 			</div>
 		)
 	}
