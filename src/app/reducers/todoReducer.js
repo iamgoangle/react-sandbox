2c71f373cb3a446c52774f6e0044700cc08a7356
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-16 11:14:00 +0700

    To fixed action and reducer
    it should not mutate previous state since it is not the same way of Redux concept

--- src/app/reducers/todoReducer.js
+++ src/app/reducers/todoReducer.js
@@ -1,13 +1,11 @@
-const initialState = {
-	users: []
-}
+const initialState = []
 
 const todoReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'GET_ALLTODO':
-			return Object.assign({}, state, { users: action.users })
+			return [...state, action.users]
 		case 'ADD_SINGLETODO':
-			return action.payload
+			return [...state, action.users]
 		default:
 			return state
 	}
