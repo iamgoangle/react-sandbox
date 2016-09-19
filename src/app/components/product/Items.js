72d0b5e9936a045e728e8476acc98fc2b1fb512b
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-19 18:20:28 +0700

    Update Items.js

--- src/app/components/product/Items.js
+++ src/app/components/product/Items.js
@@ -1,13 +1,39 @@
 import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
 
+const styles = {
+        row: {
+            margin: '5px'
+        },
+        itemBox: {
+            backgroundColor: '#e6e6e6'
+        }
+}
+
 class Items extends Component {
     render () {
         return (
-            <div className="row">
-                <div className="col-md-4">xxx</div>
-                <div className="col-md-4">xxx</div>
-                <div className="col-md-4">xxx</div>
+            <div className="row" style={styles.row}>
+                <div className="col-md-3 img-rounded">
+                    <div style={styles.itemBox}>
+                        test
+                    </div>
+                </div>
+                <div className="col-md-3 img-rounded">
+                    <div style={styles.itemBox}>
+                        test
+                    </div>
+                </div>
+                <div className="col-md-3 img-rounded">
+                    <div style={styles.itemBox}>
+                        test
+                    </div>
+                </div>
+                <div className="col-md-3 img-rounded">
+                    <div style={styles.itemBox}>
+                        test
+                    </div>
+                </div>
             </div>
         )
     }
