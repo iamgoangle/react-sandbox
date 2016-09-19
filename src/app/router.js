9e9f3ab9ebc28091cfc4ea665164bf2695b69f0e
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-19 16:40:29 +0700

    Add files via upload

--- src/app/router.js
+++ src/app/router.js
@@ -7,7 +7,7 @@ import App from './components/App'
 // containers
 import Home from './containers/pages/Home'
 import Todo from './containers/pages/Todo'
-import Rest from './containers/pages/Rest'
+import Product from './containers/pages/Product'
 import EasyRedux from './containers/pages/EasyRedux'
 
 export default (
@@ -20,9 +20,9 @@ export default (
 			<Route path='todo'
 				component={Todo}
 				title="Todo | Pull real-time data from database" />
-			<Route path='rest'
-				component={Rest}
-				title="REST APIs | Pull data from JSON REST APIS" />
+			<Route path='product'
+				component={Product}
+				title="Product | Add product to cart" />
 			<Route path='easy-redux'
 				component={EasyRedux}
 				title="Easy Redux | To learning easy redux from simple file" />
