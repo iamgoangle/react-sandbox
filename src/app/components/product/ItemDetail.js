e6b0f641831ae2c8e9ea2f5410bab1b738bb0724
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-22 17:47:48 +0700

    Add files via upload

--- src/app/components/product/ItemDetail.js
+++ src/app/components/product/ItemDetail.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
 
 import "./product.scss"
 
 class ItemDetail extends Component {
+	constructor (props, context) {
+		super(props, context)
+	}
+
+	static propTypes: {
+		title: PropTypes.string.isRequired,
+		desc: PropTypes.string.isRequired,
+		sku: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+		concurrency: PropTypes.string.isRequired,
+		thumbnail: PropTypes.string.isRequired
+	}
+
 	render () {
 		return (
-			<div className="col-md-3 img-rounded">
-				<div className="itemBox">
+			<div className="col-md-3 itemBox">
+				<div>
 					<div className="row">
-						<div className="col-md-1">
-						</div>
-						<div className="col-md-7">
-							Stay hungry, stay foolish...
+						<div className="col-md-9">
+							{this.props.title}
 						</div>
 						<div className="col-md-3 itemPrice">
-							$500
-						</div>
-						<div className="col-md-1">
+							{this.props.concurrency}{this.props.price}
 						</div>
 					</div>
 					<div className="row itemDetail">
-						<div className="col-md-1">
+						<div className="col-md-6 itemThumbnail">
+							<img src={this.props.thumbnail} />
 						</div>
-						<div className="col-md-5">
-							xxx
-						</div>
-						<div className="col-md-5">
-							asdasdsadsad
-						</div>
-						<div className="col-md-1">
+						<div className="col-md-6 itemDescription">
+							{this.props.desc}
 						</div>
 					</div>
 				</div>
--- src/app/components/product/Items.js
+++ src/app/components/product/Items.js
@@ -4,19 +4,56 @@ import { render } from 'react-dom'
 import ItemDetail from './ItemDetail'
 
 const styles = {
-        row: {
-            margin: '5px'
-        }
+    row: {
+        margin: '5px'
+    }
 }
 
 class Items extends Component {
+    constructor (state) {
+        super(state)
+
+        this.state = {}
+        this.state.products = [{
+            id: 0,
+            title: 'Beer AOI',
+            desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            sku: 'BEER-001',
+            price: '500',
+            concurrency: '$',
+            thumbnail: 'dist/assets/images/products/bottle-2.png'
+        }, {
+            id: 1,
+            title: 'Coffee',
+            desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            sku: 'COFF-001',
+            price: '100',
+            concurrency: '$',
+            thumbnail: 'dist/assets/images/products/coffee.png'
+        }, {
+            id: 2,
+            title: 'Wine soceity',
+            desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            sku: 'WINE-001',
+            price: '9,000',
+            concurrency: '$',
+            thumbnail: 'dist/assets/images/products/wine.png'
+        }]
+    }
     render () {
         return (
             <div className="row itemRow">
-                <ItemDetail product=""></ItemDetail>
-				<ItemDetail product=""></ItemDetail>
-				<ItemDetail product=""></ItemDetail>
-				<ItemDetail product=""></ItemDetail>
+                {this.state.products.map( (product, index) => (
+                    <ItemDetail
+                        key={index}
+                        title={product.title}
+                        desc={product.desc}
+                        sku={product.sku}
+                        price={product.price}
+                        concurrency={product.concurrency}
+                        thumbnail={product.thumbnail}>
+                    </ItemDetail>
+                ))}
             </div>
         )
     }
