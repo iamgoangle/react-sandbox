17423ebefa749ea04ec79c6b54659035c23a740b
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-28 14:44:53 +0700

    Add files via upload

---
+++ src/app/config/server.conf.js
@@ -0,0 +1,13 @@
+const config = {
+    DATABASE: {
+        HOST: "mongodb://localhost/golfdb"
+    },
+    SERVER: {
+        PORT: 7777
+    },
+    API: {
+        products: '/api/products'
+    }
+}
+
+export { config }
