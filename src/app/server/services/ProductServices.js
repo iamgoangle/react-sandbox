a2805d7a1466526815d2caab373af449bef09741
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-28 16:27:19 +0700

    Fixed cross origin resource sharing issue
    since the web server and api server the port is differed

--- src/app/server/services/ProductServices.js
+++ src/app/server/services/ProductServices.js
@@ -6,6 +6,9 @@ class ProductServices {
 	}
 
 	getAllProducts (req, res) {
+		res.header("Access-Control-Allow-Origin", "*")
+  		res.header("Access-Control-Allow-Headers", "X-Requested-With")
+		
 		try {
 			Product.find({}).exec(function(err, products){
 				if (err) {
@@ -21,7 +24,7 @@ class ProductServices {
 
 	addProduct (req, res) {
 		try {
-			
+
 		} catch (err) {
 			console.error(err)
 		}
