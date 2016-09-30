da5bab82f65c7f1d8a7c3d1ba34822d2d4330acb
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-30 11:15:22 +0700

    Add files via upload

--- config/server.conf.js
+++ config/server.conf.js
@@ -3,10 +3,13 @@ const config = {
         HOST: "mongodb://localhost/golfdb"
     },
     SERVER: {
+        HOST: "localhost",
         PORT: 7777
     },
+    APIKEY: "e1898b9c3134792d1affb2c6960d4f41ecfc98e9", // sha-a1("Teerapong")
     API: {
-        products: '/api/products'
+        products: '/api/products',
+        todos: '/api/todos'
     }
 }
 
